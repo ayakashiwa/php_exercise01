c6e6beafb2effa61eb9c2b90cc0f066f71d810de
ayakashiwa <ayksw122@gmail.com>
2022-02-23 18:56:24 +0900

    制御構文1の課題

---
+++ 04/if_4.php
@@ -0,0 +1,12 @@
+<?php
+
+$score_math = 30;
+$score_english = 22;
+
+if ($score_math >= 60 && $score_english >= 60) {
+    echo '合格';
+} elseif ($score_math >= 50 || $score_english >= 50) {
+    echo '再試験';
+} elseif ($score_math <= 50 && $score_english <= 50) {
+    echo '不合格';
+}
